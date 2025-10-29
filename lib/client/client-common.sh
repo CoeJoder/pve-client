4b2ed2c5a0d14e75c15f5ff0974bddd322dea228
CoeJoder <CoeJoder@users.noreply.github.com>
2025-10-28 23:23:39 -0700

    Update `client-common.sh`

--- lib/client/client-common.sh
+++ lib/client/client-common.sh
@@ -11,8 +11,10 @@
 
 # -------------------------- CONSTANTS ----------------------------------------
 
-PROJECT_NAME='pve-client'
-REMOTE_PVE_ROOT_CA='/etc/pve/pve-root-ca.pem'
+declare -r PROJECT_NAME='pve-client'
+declare -r REMOTE_PVE_ROOT_CA='/etc/pve/pve-root-ca.pem'
+declare -r REMOTE_PVECLIENT_BIN_DIR="\$HOME/.local/bin/pve-client"
+declare -r SERVER_CACHE_DIR="/var/tmp/pve-client-cache"
 
 # resolve project paths based on 'dev' (source) or 'prod' (deployment)
 #  - env is dynamically determined based on filesystem position of this script
@@ -24,70 +26,76 @@ if [[ "$(basename "$(realpath "$(dirname "${BASH_SOURCE[0]}")/..")")" == "$PROJE
 	#  prod                                 | env vars
 	# --------------------------------------|------------------------------------
 	#  ~                                    |
-	#  ├── .cache                           | _XDG_CACHE_DIR
-	#  │   └── pve-client                   |
+	#  ├── .cache/                          | _XDG_CACHE_DIR
+	#  │   └── pve-client/                  | CLIENT_CACHE_DIR
 	#  │       └── pve-root-ca.pem          | PVE_ROOT_CA
-	#  ├── .config                          | _XDG_CONFIG_DIR
-	#  │   └── pve-client                   |
+	#  ├── .config/                         | _XDG_CONFIG_DIR
+	#  │   └── pve-client/                  |
 	#  │       ├── .env                     | DOTENV
 	#  │       └── pve-api-token            | PVE_API_TOKEN
-	#  ├── .local                           |
-	#  │   └── .share                       | _XDG_DATA_DIR
-	#  │       └── pve-client               |
-	#  │           ├── external             |
-	#  │           │   └── bash-tools       |
+	#  ├── .local/                          |
+	#  │   └── .share/                      | _XDG_DATA_DIR
+	#  │       └── pve-client/              |
+	#  │           ├── external/            |
+	#  │           │   └── bash-tools/      |
 	#  │           │       └── src          | EXT_BASH_TOOLS_SRC_DIR
-	#  │           ├── client               | LIB_CLIENT_DIR
+	#  │           ├── client/              | LIB_CLIENT_DIR
 	#  |           |   └── client-common.sh | (this script)
-	#  │           └── server               | LIB_SERVER_DIR
-	#  └── .local                           |
-	#      └── bin                          | _XDG_BIN_DIR
+	#  │           ├── server/              | LIB_SERVER_DIR
+	#  |           └── global-common.sh     | 
+	#  └── .local/                          | 
+	#      └── bin/                         | _XDG_BIN_DIR
 	#          └── pve.sh                   | PVECLIENT_BIN
 
-	_XDG_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}"
-	_XDG_CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}"
-	_XDG_DATA_DIR="${XDG_DATA_HOME:-$HOME/.local/share}"
-	_XDG_BIN_DIR="$HOME/.local/bin"
+	declare -r _XDG_CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}"
+	declare -r _XDG_CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}"
+	declare -r _XDG_DATA_DIR="${XDG_DATA_HOME:-$HOME/.local/share}"
+	declare -r _XDG_BIN_DIR="$HOME/.local/bin"
 
-	PVE_ROOT_CA="$_XDG_CACHE_DIR/$PROJECT_NAME/pve-root-ca.pem"
-	DOTENV="$_XDG_CONFIG_DIR/$PROJECT_NAME/.env"
-	EXT_BASH_TOOLS_SRC_DIR="$_XDG_DATA_DIR/$PROJECT_NAME/external/bash-tools/src"
-	LIB_CLIENT_DIR="$_XDG_DATA_DIR/$PROJECT_NAME/client"
-	LIB_SERVER_DIR="$_XDG_DATA_DIR/$PROJECT_NAME/server"
-	PVECLIENT_BIN="$_XDG_BIN_DIR/pve.sh"
+	declare -r CLIENT_CACHE_DIR="$_XDG_CACHE_DIR/$PROJECT_NAME"
+	declare -r PVE_ROOT_CA="$CLIENT_CACHE_DIR/pve-root-ca.pem"
+	declare -r DOTENV="$_XDG_CONFIG_DIR/$PROJECT_NAME/.env"
+	declare -r EXT_BASH_TOOLS_SRC_DIR="$_XDG_DATA_DIR/$PROJECT_NAME/external/bash-tools/src"
+	declare -r LIB_CLIENT_DIR="$_XDG_DATA_DIR/$PROJECT_NAME/client"
+	declare -r LIB_SERVER_DIR="$_XDG_DATA_DIR/$PROJECT_NAME/server"
+	declare -r PVECLIENT_BIN="$_XDG_BIN_DIR/pve.sh"
 
 	# TODO delete this and always generate on-the-fly
-	PVE_API_TOKEN="$_XDG_CONFIG_DIR/$PROJECT_NAME/pve-api-token"
+	declare -r PVE_API_TOKEN="$_XDG_CONFIG_DIR/$PROJECT_NAME/pve-api-token"
 else
 	#  dev                                  | env vars
 	# --------------------------------------|------------------------------------
-	#  pve-client                           | _PROJ_DIR
-	#  ├── external                         |
-	#  │   └── bash-tools                   |
+	#  pve-client/                          | _PROJ_DIR
+	#  ├── external/                        |
+	#  │   └── bash-tools/                  |
 	#  │       └── src                      | EXT_BASH_TOOLS_SRC_DIR
-	#  ├── lib                              |
-	#  │   ├── client                       | LIB_CLIENT_DIR
+	#  ├── lib/                             |
+	#  │   ├── client/                      | LIB_CLIENT_DIR
 	#  |   |   └── client-common.sh         | (this script)
-	#  │   └── server                       | LIB_SERVER_DIR
-	#  ├── src                              |
+	#  │   └── server/                      | LIB_SERVER_DIR
+	#  |   └── global-common.sh             | 
+	#  ├── src/                             |
 	#  │   └── pve.sh                       | PVECLIENT_BIN
-	#  ├── tools                            | _TOOLS_DIR
+	#  ├── tools/                           | _TOOLS_DIR
+	#  ├── cache/                           | CLIENT_CACHE_DIR
+	#  │   └── pve-root-ca.pem              | PVE_ROOT_CA
 	#  ├── .env                             | DOTENV
 	#  ├── pve-api-token                    | PVE_API_TOKEN
-	#  └── pve-root-ca.pem                  | PVE_ROOT_CA
 
 	_PROJ_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")/../..")"
-	_TOOLS_DIR="$_PROJ_DIR/tools"
+	declare -r _PROJ_DIR
+	declare -r _TOOLS_DIR="$_PROJ_DIR/tools"
 
-	PVE_ROOT_CA="$_PROJ_DIR/pve-root-ca.pem"
-	DOTENV="$_PROJ_DIR/.env"
-	EXT_BASH_TOOLS_SRC_DIR="$_PROJ_DIR/external/bash-tools/src"
-	LIB_CLIENT_DIR="$_PROJ_DIR/lib/client"
-	LIB_SERVER_DIR="$_PROJ_DIR/lib/server"
-	PVECLIENT_BIN="$_PROJ_DIR/src/pve.sh"
+	declare -r CLIENT_CACHE_DIR="$_PROJ_DIR/cache"
+	declare -r PVE_ROOT_CA="$CLIENT_CACHE_DIR/pve-root-ca.pem"
+	declare -r DOTENV="$_PROJ_DIR/.env"
+	declare -r EXT_BASH_TOOLS_SRC_DIR="$_PROJ_DIR/external/bash-tools/src"
+	declare -r LIB_CLIENT_DIR="$_PROJ_DIR/lib/client"
+	declare -r LIB_SERVER_DIR="$_PROJ_DIR/lib/server"
+	declare -r PVECLIENT_BIN="$_PROJ_DIR/src/pve.sh"
 
 	# TODO delete this and always generate on-the-fly
-	PVE_API_TOKEN="$_PROJ_DIR/pve-api-token"
+	declare -r PVE_API_TOKEN="$_PROJ_DIR/pve-api-token"
 fi
 
 # -------------------------- IMPORTS ------------------------------------------
@@ -105,17 +113,24 @@ assert_sourced
 # set the project environment variables
 function set_env() {
 	reset_checks
-	check_file_exists dotenv
+	check_file_exists DOTENV
 	print_failed_checks --error || return
 
 	# shellcheck source=.env
 	source "$DOTENV"
+}
+readonly -f set_env
+
+# common script setup tasks
+function housekeeping() {
+	set_env
 }
 
 # test whether this is dev env
 function is_devmode() {
 	[[ -n $_TOOLS_DIR ]] &>/dev/null
 }
+readonly -f is_devmode
 
 # -------------------------- ASSERTIONS ---------------------------------------
 
