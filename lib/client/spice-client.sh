458a5864577c230678edab66dbce0e5baff1e610
CoeJoder <CoeJoder@users.noreply.github.com>
2025-11-12 14:52:43 -0800

    Finish `spice-client.sh`

--- lib/client/spice-client.sh
+++ lib/client/spice-client.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 #
 # spice-client.sh
-# Launch a SPICE client connected to the VM or container by the following:
+# Launch a SPICE client connection to VM or container.
 #
-# 1. SSH into the Proxmox host and:
-#   a. Set the QXL display driver if not already.
-#   b. Start & stop the VM if it was running while the setting was changed.
-#   c. Generate a secure, one-time SPICE ticket.
-# 2. Then locally:
-#   a. Convert the JSON ticket into a VirtViewer INI temp file.
-#   b. Launch VirtViewer GUI with the INI file, which is then deleted.
+# Algorithm:
+#   1a. If SPICE is not enabled:
+#     a. If VM is running, stop it.
+#     b. Enable SPICE driver.
+#     c. Start the VM.
+#   1b. Else:
+#     a. If VM is stopped, start it.
+#   2. Generate a secure, one-time SPICE ticket.
+#   3. Convert the SPICE ticket JSON into a VirtViewer INI temp file.
+#   4. Launch VirtViewer GUI with the INI file, which is then deleted.
 #
 # Usage:
 #   spice-client.sh [options] <vm>
 # -------------------------- HEADER -------------------------------------------
-
-# set -eEo pipefail
-# shopt -s inherit_errexit
 
 this_dir="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 source "$this_dir/client-commons.sh"
@@ -26,26 +26,33 @@ function show_usage() {
 	cat >&2 <<-EOF
 		Usage: $(basename "${BASH_SOURCE[0]}") [options] <guest-name>
 		Options:
-		--timeout ${underline}val${nounderline}   The VM restart timeout in seconds (default: 15)
-		--no-banner     Skip banner display
-		--help, -h      Show this message
+		--timeout ${underline}val${nounderline}     The VM restart timeout in seconds (default: 15)
+		--log-level ${underline}val${nounderline}   The log-level (default: info)
+		--no-banner       Skip banner display
+		--help, -h        Show this message
 	EOF
 }
 
 _parsed_args=$(getopt \
 	--options='h,t:' \
-	--longoptions='help,no-banner,timeout:' \
+	--longoptions='help,no-banner,timeout:,log-level:' \
 	--name "$(basename "${BASH_SOURCE[0]}")" -- "$@")
 eval set -- "$_parsed_args"
 unset _parsed_args
 
 timeout=15
+loglevel=info
 no_banner=0
 
 while true; do
 	case "$1" in
 	-t | --timeout)
 		timeout="$2"
+		shift 2
+		continue
+		;;
+	--log-level)
+		loglevel="$2"
 		shift 2
 		continue
 		;;
@@ -83,6 +90,7 @@ assert_not_sourced
 reset_checks
 check_is_defined guest
 check_is_positive_integer timeout
+check_is_valid_loglevel loglevel
 
 check_is_defined PVE_HOST
 check_is_valid_port PVE_PORT
@@ -111,88 +119,94 @@ fi
 
 # -------------------------- RECONNAISSANCE -----------------------------------
 
+set_loglevel "$loglevel"
+
+vmid="$(get_guest_id "$guest")"
+
 # -------------------------- EXECUTION ----------------------------------------
 
-# Enable SPICE for VM by setting the display adapter to 'qxl'.
-function set_display_spice() {
+# Test if 'qxl' display driver is set.
+function is_display_spice() {
+	functrace
 	local qm_config
 
-	log info "Checking VGA config..."
-	qm_config="$(ssh "$PVE_SSH_HOST" "sudo qm config '$vmid'")" || return
+	log info "Checking if SPICE driver is set..."
+	qm_config="$(manage_guest config "$vmid")" || return
 	if ! grep -q '^vga: qxl' <<<"$qm_config"; then
-		log info 'Setting QXL display driver...'
-		ssh "$PVE_SSH_HOST" "sudo qm set '$vmid' --vga qxl --memory 32" || return
+		log info 'SPICE driver not set.'
+		return 1
 	fi
+}
+
+# Set the 'qxl' display driver.
+function set_display_spice() {
+	functrace
+
+	log info 'Setting display to SPICE...'
+	manage_guest set "$vmid" --vga qxl || return
 }
 
 # Get the SPICE ticket in JSON format via pvesh
 function get_spice_ticket() {
-	log ingo "Requesting SPICE ticket via pvesh..."
-	if ! ssh "$PVE_SSH_HOST" "sudo pvesh create "/nodes/$PVE_NODE/qemu/$vmid/spiceproxy" --output-format json-pretty"; then
+	functrace
+
+	log info "Requesting SPICE ticket via pvesh..."
+	# if ! ssh "$PVE_SSH_HOST" "sudo pvesh create "/nodes/$PVE_NODE/qemu/$vmid/spiceproxy" --output-format json-pretty"; then
+	if ! pvesh create "/nodes/$PVE_NODE/qemu/$vmid/spiceproxy" --output-format json-pretty; then
 		log error "Failed to retrieve SPICE ticket."
 		return 1
 	fi
 }
 
-function main() {
-	local status
-	local spice_json
-	local vv_temp_file
+# Stop VM and wait for it to be stopped.
+function stop_vm() {
+	functrace
 
-	set_display_spice "$vmid" "$timeout" || return
-	
-	status="$(get_vm_status "$vmid")"
-	if [[ "$status" == "$VM_STATUS_STOPPED" ]]; then
-		log info 'Starting VM...'
-		ssh "$PVE_SSH_HOST" "sudo qm start '$vmid'" || return
-		wait_until_vm_is_running "$vmid" "$timeout" || return
-	elif [[ "$status" == "$VM_STATUS_RUNNING" ]]; then
-		log info 'Restarting VM to apply changes...'
-		sudo qm stop "$vmid" || return
-		wait_until_vm_is_stopped "$vmid" "$timeout" || return
-		sudo qm start "$vmid" || return
-		wait_until_vm_is_running "$vmid" "$timeout" || return
-	else
-		log warn "VM is ${status}."
-		yes_or_no --default-yes "Launch SPICE client anyway?" || return
-	fi
-	
-	spice_json="$(get_spice_ticket)" || return
-	vv_temp_file="$(mktemp)" || return
-	echo "$spice_json" | jq -r 'def kv: to_entries[] | "\(.key)=\(.value)"; "[virt-viewer]", kv' >"$vv_temp_file"
-	if [[ ! -s "$vv_temp_file" ]]; then
-		echo "✗ Failed to create VirtViewer connection file." >&2
-		return 1
-	fi
-
-	echo "→ Launching VirtViewer..."
-	remote-viewer -- "$vv_temp_file" &
+	log info "Stopping VM..."
+	manage_guest stop "$vmid" || return
+	wait_until_vm_is_stopped "$vmid" "$timeout" || return
 }
 
+# Start VM and wait for it to be running.
+function start_vm() {
+	functrace
+
+	log info 'Starting VM...'
+	manage_guest start "$vmid" || return
+	wait_until_vm_is_running "$vmid" "$timeout" || return
+}
+
+set -E
 trap 'on_err' ERR
 
+status="$(get_vm_status "$vmid")"
 
-if [[ -z "$HOST" || -z "$NODE" || -z "$VMID" || -z "$USER" ]]; then
-	cat "Usage: pve_client_utils__connect_vm_spice " >&2
-	return 1
+if ! is_display_spice; then
+	if [[ "$status" == "$VM_STATUS_RUNNING" ]]; then
+		stop_vm || exit
+	fi
+	set_display_spice || exit
+	start_vm || exit
+else
+	if [[ "$status" == "$VM_STATUS_STOPPED" ]]; then
+		start_vm || exit
+	fi
 fi
 
-echo "
-	set -e
-	source ~/scripts/common.sh || exit
-	source ~/scripts/pve_host_utils.sh || exit
+spice_json="$(get_spice_ticket)" || exit
+vv_temp_file="$(mktemp)" || exit
+echo "$spice_json" | jq -r 'def kv: to_entries[] | "\(.key)=\(.value)"; "[virt-viewer]", kv' >"$vv_temp_file"
+if [[ ! -s "$vv_temp_file" ]]; then
+	log error "Failed to create VirtViewer connection file."
+	exit 1
+fi
 
-	pve_host_utils__set_display_spice '$vmid' '$timeout' || exit
-	pve_host_utils__get_spice_ticket '$NODE' '$vmid' || exit
-	"
-
-# spice_json="$(ssh "$PVE_SSH_HOST" "
-# 	$_pve_client_utils__host_init_script
-
-# 	pve_host_utils__set_display_spice '$vmid' '$timeout' || exit
-# 	pve_host_utils__get_spice_ticket '$NODE' '$vmid' || exit
-# 	")" || return
-
-
+log info "Launching VirtViewer..."
+if ! (remote-viewer -- "$vv_temp_file" &>/dev/null &); then
+	# Ensure temp file is deleted even if remote-viewer fails.
+	if [[ -n "$vv_temp_file" ]]; then
+		rm -rf --interactive=never "$vv_temp_file" &>/dev/null
+	fi
+fi
 
 # -------------------------- POSTCONDITIONS -----------------------------------
